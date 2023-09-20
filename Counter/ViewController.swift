13bbd65fe59ffaa91110f967a59c6d20c0640abd
Sergey Telnov <sergej.telnov@gmail.com>
2023-09-20 12:50:54 +0200

    Первая версия счетчика, упрощенная. Добавлено изменение размера шрифта, при изменении текста в label.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonCounter: UIButton!
+    @IBOutlet weak var labelCounter: UILabel!
+    private var valueCounter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelCounter.text = "Значение счётчика: \(valueCounter)"
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        valueCounter += 1
+        labelCounter.text = "Значение счётчика: \(valueCounter)"
+    }
+    
 }
 
