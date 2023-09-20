9396e0afe6148dc66b203dadd81c8892bf8f83a7
Sergey Telnov <sergej.telnov@gmail.com>
2023-09-20 13:30:17 +0200

    - Добавлена кнопка МИНУС - Найден баг, связанный с неправильным отображением цветов кнопок при загрузке - исправлен прямым указанием нужного цвета из кода

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,19 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var buttonCounter: UIButton!
+    @IBOutlet weak var buttonMinus: UIButton!
+    @IBOutlet weak var buttonPlus: UIButton!
     @IBOutlet weak var labelCounter: UILabel!
     private var valueCounter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         labelCounter.text = "Значение счётчика: \(valueCounter)"
+        buttonPlus.tintColor = .blue
+        buttonMinus.tintColor = .red
     }
 
-    @IBAction func buttonPressed(_ sender: Any) {
+    @IBAction func plusButtonPressed(_ sender: Any) {
         valueCounter += 1
         labelCounter.text = "Значение счётчика: \(valueCounter)"
     }
     
+    @IBAction func minusButtonPressed(_ sender: Any) {
+        valueCounter -= 1
+        if valueCounter < 0 { valueCounter = 0 }
+        labelCounter.text = "Значение счётчика: \(valueCounter)"
+    }
 }
 
